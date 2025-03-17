f16444874243ebc10846e76503710a40dda369a2
Ridzz05 <muhrizkialghipari@gmail.com>
2025-03-17 21:46:10 +0700

    update: Perubahan terbaru

--- src/app/components/Navbar.tsx
+++ src/app/components/Navbar.tsx
@@ -18,6 +18,7 @@ const Navbar = () => {
     const handleClearSearch = () => {
         setLocalSearchQuery('');
         setSearchQuery('');
+        setShowSearchModal(false); // Menutup modal pencarian setelah dibersihkan
     };
 
     const handleSearchSubmit = () => {
