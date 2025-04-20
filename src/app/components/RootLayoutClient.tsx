791072308e1fc791940f83f9738fa378f2b751c2
Ridzz05 <muhrizkialghipari@gmail.com>
2025-04-21 04:13:58 +0700

    update: Perubahan terbaru

--- src/app/components/RootLayoutClient.tsx
+++ src/app/components/RootLayoutClient.tsx
@@ -1,27 +1,29 @@
 'use client'
 
-import { useEffect } from 'react'
-import Navbar from './Navbar'
 import { SearchProvider } from '../context/SearchContext'
+import Navbar from './Navbar'
+
+interface RootLayoutClientProps {
+    children: React.ReactNode;
+    lexendGigaClass: string;
+    lexendClass: string;
+    montserratClass: string;
+    ralewayClass: string;
+}
 
 export default function RootLayoutClient({
     children,
-}: {
-    children: React.ReactNode
-}) {
-    useEffect(() => {
-        // Menambahkan class untuk mencegah scroll saat modal terbuka
-        document.documentElement.classList.add('scroll-smooth');
-        
-        return () => {
-            document.documentElement.classList.remove('scroll-smooth');
-        };
-    }, []);
-
+    lexendGigaClass,
+    lexendClass,
+    montserratClass,
+    ralewayClass
+}: RootLayoutClientProps) {
     return (
-        <SearchProvider>
-            <Navbar />
-            {children}
-        </SearchProvider>
+        <body className={`${lexendGigaClass} ${lexendClass} ${montserratClass} ${ralewayClass} font-montserrat bg-[#FFFBF2]`}>
+            <SearchProvider>
+                <Navbar />
+                {children}
+            </SearchProvider>
+        </body>
     )
 } 
